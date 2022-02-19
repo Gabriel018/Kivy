5f6cfa7a2aa6cb6f20067c9ebc0830e8ae1f8403
Gabriel018 <duke.comandos@gmail.com>
2022-02-19 10:16:10 -0300

    1.1

--- Teste.py
+++ Teste.py
@@ -1,10 +1,20 @@
 import kivy
 from  kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 class Teste(App):
     def build(self):
-        return Label(text='Ola Mundo')
+
+     grad = BoxLayout(orientation='vertical')
+     btn = Button(text='Clicar',on_release=self.alterar)
+     self.lbl = Label(text='Ola Mundo')
+     grad.add_widget(self.lbl)
+     grad.add_widget(btn)
+     return grad
+    def alterar(self,lbl):
+        self.lbl.text='Alterado'
 
 
 Teste().run()
