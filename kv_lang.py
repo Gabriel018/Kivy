0a2718dbfdb84c499740c0e32cd7b23aeb39ddad
Gabriel018 <duke.comandos@gmail.com>
2022-02-28 22:26:21 -0300

    1.1

--- kv_lang.py
+++ kv_lang.py
@@ -14,10 +14,21 @@ GridLayout:
     spacing:20
     Label:
         text:"Digite seu nome"
-    TextInput:          
+        size_hint_x: 1
+        size_hint_y: None
+        font_size: 25
+        
+    TextInput:
+        size_hint_x: 1
+        size_hint_y: None
+        height: 40          
     Label:
         text:"Digite sua senha"
+        font_size: 25
+        size_hint: (1, None)
     TextInput:   
+        size_hint: (1, None)
+        height: 40  
      '''
 
 class lang(App):
