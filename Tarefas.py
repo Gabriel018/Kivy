d65c07e295a8e3da274f467a9a6e595ea0a45607
Gabriel018 <duke.comandos@gmail.com>
2022-03-03 10:14:48 -0300

    1.1

--- Tarefas.py
+++ Tarefas.py
@@ -1,16 +1,17 @@
 from kivy.app import  App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 
 
-class add_tarefa(BoxLayout):
+class add_tarefa(ScrollView):
     def __init__(self,tarefas):
         super().__init__()
         for tarefa in tarefas:
-            self.add_widget(Label(text=tarefa))
+            self.ids.Caixa.add_widget(Label(text=tarefa,font_size=20,size_hint_y=None,height=200))
 
 class Teste(App):
     def build(self):
-        return add_tarefa(["Estudar",'Comer'])
+        return add_tarefa(["Estudar",'Comer','fazer sexo,','ler','Musculaçao'])
 
 Teste().run()
