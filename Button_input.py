7e227be9eb105408809834a67dca0b34313711fc
Gabriel018 <duke.comandos@gmail.com>
2022-03-18 09:59:28 -0300

    1.1

---
+++ Button_input.py
@@ -0,0 +1,40 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MyLayout(GridLayout):
+    def __init__(self,**kwargs):
+        super(MyLayout,self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Name: '))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text='Last_Name: '))
+        self.last_name = TextInput(multiline=False)
+        self.add_widget(self.last_name)
+
+        self.add_widget(Label(text='Curso: '))
+        self.curso = TextInput(multiline=False)
+        self.add_widget(self.curso)
+
+        self.Enviar = Button(text='Enviar')
+        self.Enviar.bind(on_press=self.press)
+        self.add_widget(self.Enviar)
+
+    def press(self,instance):
+        name = self.name.text
+        last_name = self.last_name.text
+        curso = self.curso.text
+
+        print(f'Boa tarde,{name}{last_name}, seu curso escolhido foi,{curso}')
+
+class Myapp(App):
+    def build(self):
+        return  MyLayout()
+
+if __name__ == '__main__':
+    Myapp().run()
